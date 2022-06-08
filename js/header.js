e1800e09f437699beb53f3d3d2820fe6c314ea14
braceyourself0818 <85159654+junny1995@users.noreply.github.com>
2022-06-08 20:19:59 +0900

    Update to-top icon

--- js/header.js
+++ js/header.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener("blur", function() {
 });
 
 const badgeEl = document.querySelector(".badges");
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener("scroll", _.throttle(function (){
   console.log(window.scrollY);
@@ -26,12 +27,26 @@ window.addEventListener("scroll", _.throttle(function (){
       opacity: 0,
       display: "none"
     });
+    //버튼 숨기기
+    gsap.to(toTopEl, 0.2, {
+      x: 0
+    });
   } else {
     //배지 보이기
     gsap.to(badgeEl, 0.4, {
       opacity: 1,
       display: "block"
     });
+    //버튼 숨기기
+    gsap.to(toTopEl, 0.2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0
+  });
+})
